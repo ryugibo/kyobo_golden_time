21f007c516f64a7ddd9a3ca7b619ce404228e5b2
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-07-09 06:37:02 +0900

    new mix project?

---
+++ lib/kyobo_golden_time.ex
@@ -0,0 +1,18 @@
+defmodule KyoboGoldenTime do
+  @moduledoc """
+  Documentation for `KyoboGoldenTime`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KyoboGoldenTime.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kyobo_golden_time_test.exs
@@ -0,0 +1,8 @@
+defmodule KyoboGoldenTimeTest do
+  use ExUnit.Case
+  doctest KyoboGoldenTime
+
+  test "greets the world" do
+    assert KyoboGoldenTime.hello() == :world
+  end
+end
