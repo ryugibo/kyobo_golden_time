ad8e0f6cadc4dc2a6f98ab3abbd0082306421e60
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-07-10 00:39:24 +0900

    이전 시간과 바뀐 경우에만 노티되게 수정

--- lib/kyobo_golden_time.ex
+++ lib/kyobo_golden_time.ex
@@ -3,14 +3,37 @@ defmodule KyoboGoldenTime do
   Documentation for `KyoboGoldenTime`.
   """
 
+  @prev_directory "tmp/"
+  @prev_file_path "#{@prev_directory}prev_result"
+
+  defp get_prev_result() do
+    unless File.exists?(@prev_directory) do
+      File.mkdir(@prev_directory)
+    end
+
+    File.read(@prev_file_path)
+    |> case do
+      {:ok, prev_time} ->
+        prev_time
+
+      _ ->
+        nil
+    end
+  end
+
   def fetch() do
+    prev_time = get_prev_result()
+
     with %{status_code: 200, body: body} <-
            Crawly.fetch("https://www.kyobobook.co.kr/api/gw/onk/benefit/checkGoldenTimeNow"),
          {:ok, %{data: %{type: "READY", startTime: startTime}}} <-
-           Jason.decode(body, keys: :atoms) do
+           Jason.decode(body, keys: :atoms),
+         true <- prev_time != startTime || {:error, :not_changed} do
+      File.write!("tmp/prev_result", startTime)
       startTime
     else
-      _ ->
+      o ->
+        o |> IO.inspect(label: "error")
         "error"
     end
     |> IO.puts()
